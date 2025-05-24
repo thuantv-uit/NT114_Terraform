9415c2306a0dba7a9ea01fdce09368f17d38e118
tvt04102004@gmail.com <tvt04102004@gmail.com>
2025-05-24 23:58:56 +0700

    feat(config): add feauter for ec2

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -7,14 +7,41 @@ resource "aws_instance" "public" {
 
   user_data = <<-EOF
               #!/bin/bash
-              # Update package index
+              # Tạo thư mục thuatv trong /
+              mkdir -p /thuatv
+
+              # Đăng nhập với quyền root
+              sudo -i
+
+              # Chuyển đến thư mục /thuatv
+              cd /thuatv
+
+              # Cập nhật danh sách gói
               apt update
 
-              # Install Docker
-              apt install docker.io
+              # Cài đặt Docker
+              apt install -y docker.io
 
-              # Install Docker Compose
-              apt install docker-compose
+              # Khởi động Docker service
+              systemctl start docker
+              systemctl enable docker
+
+              # Kéo các Docker images
+              docker pull thuan410/nt114-frontend:v2
+              docker pull thuan410/nt114-be-user:v2
+              docker pull thuan410/nt114-be-board:v2
+              docker pull thuan410/nt114-be-column:v2
+              docker pull thuan410/nt114-be-card:v2
+              docker pull thuan410/nt114-be-invitation:v2
+
+              # Chạy Docker Run
+              docker run -d --name frontend -p 3000:3000 thuan410/nt114-frontend:v2
+              docker run -d --name be-user -p 3001:3001 thuan410/nt114-be-user:v2
+              docker run -d --name be-board -p 3002:3002 thuan410/nt114-be-board:v2
+              docker run -d --name be-column -p 3003:3003 thuan410/nt114-be-column:v2
+              docker run -d --name be-card -p 3004:3004 thuan410/nt114-be-card:v2
+              docker run -d --name be-invitation -p 3005:3005 thuan410/nt114-be-invitation:v2
+
               EOF
 
   tags = {
@@ -31,14 +58,41 @@ resource "aws_instance" "private" {
 
   user_data = <<-EOF
               #!/bin/bash
-              # Update package index
+              # Tạo thư mục thuatv trong /
+              mkdir -p /thuatv
+
+              # Đăng nhập với quyền root
+              sudo -i
+
+              # Chuyển đến thư mục /thuatv
+              cd /thuatv
+
+              # Cập nhật danh sách gói
               apt update
+              
+              # Cài đặt Docker
+              apt install -y docker.io
 
-              # Install Docker
-              apt install docker.io
+              # Khởi động Docker service
+              systemctl start docker
+              systemctl enable docker
 
-              # Install Docker Compose
-              apt install docker-compose
+              # Kéo các Docker images
+              docker pull thuan410/nt114-frontend:v2
+              docker pull thuan410/nt114-be-user:v2
+              docker pull thuan410/nt114-be-board:v2
+              docker pull thuan410/nt114-be-column:v2
+              docker pull thuan410/nt114-be-card:v2
+              docker pull thuan410/nt114-be-invitation:v2
+
+              # Chạy Docker Run
+              docker run -d --name frontend -p 3000:3000 thuan410/nt114-frontend:v2
+              docker run -d --name be-user -p 3001:3001 thuan410/nt114-be-user:v2
+              docker run -d --name be-board -p 3002:3002 thuan410/nt114-be-board:v2
+              docker run -d --name be-column -p 3003:3003 thuan410/nt114-be-column:v2
+              docker run -d --name be-card -p 3004:3004 thuan410/nt114-be-card:v2
+              docker run -d --name be-invitation -p 3005:3005 thuan410/nt114-be-invitation:v2
+
               EOF
 
   tags = {
